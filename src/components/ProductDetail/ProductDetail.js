9bbb0813b4861f49bbef6018ca9ca6050eb2fddc
azizulhakim0794 <67514785+azizulhakim0794@users.noreply.github.com>
2020-11-28 15:03:54 +0600

    Update ProductDetail.js

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -9,7 +9,7 @@ const ProductDetail = () => {
     const [product, setProduct] = useState(null);
     
     useEffect(() =>{
-        fetch('http://localhost:4200/product/'+ productKey)
+        fetch('http://fast-fjord-97929.herokuapp.com/product/'+ productKey)
         .then(res => res.json())
         .then(data => {
             setProduct(data);
